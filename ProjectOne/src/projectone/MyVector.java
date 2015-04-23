96454e44bf568745ecd7c5f43b2c0fc777894517
Cosmos-it <taban@tabancosmos.com>
2015-04-23 11:01:42 -0700

    Added new project

--- ProjectOne/src/projectone/MyVector.java
+++ ProjectOne/src/projectone/MyVector.java
@@ -1,17 +1,134 @@
 package projectone;
+
+import java.util.ArrayList;
+
 /**
+ *
  * @author Taban
+ * 
+ * This is the expected solution
+ *  x        =  (3.0, 2.0, 2.0, 22.0)
+ *  y        =  (55.0, 22.0, 42.0, .0)
+ *  x + y    =  (2.0, 5.0, 7.0, 5.0)
+ *  10x      =  (1.0, 20.0, 5.0, 10.0)
+ *  |x|      =  5.15
+ *  <x, y>   =  35.0
+ *  |x - y|  =  5.55
+ * 
+ * This vector class is not a java vector it
+ * is built from scratch!
+ *
  */
 public class MyVector {
 
-    /**
-     * Store the vectors in ArrayList
-     */
+    private final ArrayList<Double> data;
+    private final int N;
 
-    public MyVector() {
-        
+    public MyVector(double[] data) {
+        N = data.length;
+        // defensive copy so that client can't alter our copy of data[]
+        this.data = new ArrayList<>(N);
+        for (int i = 0; i < N; i++) {
+            this.data.add(data[i]);
+        }
     }
-    
-    
-    
+
+    // create the zero vector of length N
+    public MyVector(int N) {
+        this.N = N;
+        this.data = new ArrayList<>(N);
+    }
+
+    // return the length of the vector
+    public int length() {
+        return N;
+    }
+
+    // return the inner product of this Vector a and b
+    public double dot(MyVector thatVector) {
+        if (this.N != thatVector.N) {
+            throw new RuntimeException("Dimensions don't agree");
+        }
+        double sum = 0.0;
+        for (int i = 0; i < N; i++) {
+            sum = sum + (this.data.get(i) * thatVector.data.get(i));
+        }
+        return sum;
+    }
+
+    // return this + thatVector
+    public MyVector plus(MyVector thatVector) {
+        if (this.N != thatVector.N) {
+            throw new RuntimeException("Dimensions don't agree");
+        }
+        MyVector c = new MyVector(N);
+        for (int i = 0; i < N; i++) {
+            c.data.add(this.data.get(i) + thatVector.data.get(i));
+        }
+        return c;
+    }
+
+    // return this - thatVector
+    public MyVector minus(MyVector thatVector) {
+        if (this.N != thatVector.N) {
+            throw new RuntimeException("Dimensions don't agree");
+        }
+        MyVector c = new MyVector(N);
+        for (int i = 0; i < N; i++) {
+            c.data.add(this.data.get(i) - thatVector.data.get(i));
+        }
+        return c;
+    }
+
+    //create and return a new object whose value is (this * factor)
+    public MyVector multiply(double factor) {
+        MyVector c = new MyVector(N);
+        for (int i = 0; i < N; i++) {
+            c.data.add(factor * this.data.get(i));
+        }
+        return c;
+    }
+
+    //scale the values in the array. (Multiply by a double)
+    public MyVector scaledBy(double scale) {
+        double[] sum = new double[data.size()];
+        for (int i = 0; i < data.size(); i++) {
+            sum[i] = scale * this.data.get(i);
+        }
+        return new MyVector(sum);
+    }
+
+    //return absolute value |abs|
+    public MyVector abs(MyVector vector) {
+        double[] absValue = new double[data.size()];
+        for (int i = 0; i < this.data.size(); i++) {
+            absValue[i] = Math.sqrt(Math.pow(vector.data.get(i), 2));
+        }
+        return new MyVector(absValue);
+
+    }
+
+    //check the equality (Length) for the vectors
+    public boolean equals(MyVector vector) {
+        for (int i = 0; i < data.size(); i++) {
+            if (!vector.data.get(i).equals(data.get(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //return a string representation of the vector
+    @Override
+    public String toString() {
+        String outPut = "{";
+        for (int i = 0; i < N; i++) {
+            outPut += data.get(i);
+            if (i < N - 1) {
+                outPut += ", ";
+            }
+        }
+        return outPut + "}";
+    }
+
 }
--- ProjectOne/src/projectone/ProjectOne.java
+++ ProjectOne/src/projectone/ProjectOne.java
@@ -1,14 +1,18 @@
 package projectone;
 
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * @project: ProjectOne
  * @account: CS30100_28
- * @author Taban
- * @creation date: 4/10/15
- * @completion time:
- * 
- * @Honor code: I pledge that this program represents my own
- *  program code. 
+ * @author Taban Cosmos
+ * @creation date: 4/20/15
+ * @completion time: 7hours with periodic stops
+ *
+ * @Honor code: I pledge that this program represents my own program code.
  */
 public class ProjectOne {
 
@@ -16,8 +20,83 @@ public class ProjectOne {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-       
+        //Using Lists to store inputs
+        //Then storing the inputs into double array
+        List vector1 = new ArrayList<>();
+        List vector2 = new ArrayList<>();
+        double c1 = 0;
+        double c2 = 0;
+        boolean inPut1 = true;
+        boolean inPut2 = true;
+        printOut(c1, vector1, inPut1, c2, vector2, inPut2);
+
     }
-    
+
+    //This method prints out the output being calculated in
+    //the MyVector class
+    private static void printOut(double c1, List vector1,
+            boolean inPut1, double c2, List vector2, boolean inPut2) {
+        double multiplier;
+        Scanner sc = new Scanner(System.in);
+
+        //First coordinates input
+        System.out.println("Enter Multiplier for scalar product: ");
+        multiplier = sc.nextDouble();
+        System.out.println("Enter vector 1 cordinates or enter"
+                + " 0 to finish: ");
+        do {
+            try {
+                c1 = sc.nextDouble();
+            } catch (InputMismatchException e) {
+                System.out.println("Please Enter a valid "
+                        + "Number for Vector 1: ");
+                sc = new Scanner(System.in);
+                sc.nextDouble();
+            }
+            if (c1 == 0) {
+                break;
+            }
+            vector1.add(c1);
+        } while (inPut1);
+        double[] initvalues1 = new double[vector1.size()];
+        for (int i = 0; i < initvalues1.length; i++) {
+
+            initvalues1[i] = (double) vector1.get(i);
+        }
+
+        //Second coordinates input
+        System.out.println("Enter vector 2 coordinates: 0 for done!");
+        do {
+            try {
+                c2 = sc.nextDouble();
+            } catch (InputMismatchException e) {
+                System.out.println("Enter Valid coordinates for vector2: ");
+                sc = new Scanner(System.in);
+                sc.nextDouble();
+            }
+            if (c2 == 0) {
+                break;
+            }
+            vector2.add(c2);
+        } while (inPut2);
+        double initvalues2[] = new double[vector2.size()];
+        for (int i = 0; i < vector2.size(); i++) {
+            initvalues2[i] = (double) vector2.get(i);
+        }
+        MyVector x = new MyVector(initvalues1);
+        MyVector y = new MyVector(initvalues2);
+
+        System.out.println(x.toString());
+        System.out.println(y.toString());
+        System.out.println("Vector equals: " + x.equals(y));
+        System.out.println("Vector dot: " + x.dot(y));
+        System.out.println("Vector minus: " + x.minus(y));
+        System.out.println("Vector plus: " + x.plus(y));
+        System.out.println("Vector scaleby: " + x.scaledBy(multiplier));
+        System.out.println("Vector scaleby: " + y.scaledBy(multiplier));
+        System.out.println("Vector abs: " + x.abs(x));
+        System.out.println("Vector abs: " + y.abs(y));
+
+    }
+
 }
